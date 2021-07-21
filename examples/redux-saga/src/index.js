b3bdbebf28191beb76e726d25c17e1b290885392
Jaspaul Aggarwal <jaspaul.aggarwal@akinova.com>
2021-07-21 17:11:43 +0100

    revert file for test

--- examples/redux-saga/src/index.js
+++ examples/redux-saga/src/index.js
@@ -1,23 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
 import "./styles.css";
-// import makeStore from "./store";
-// import App from "./App";
+import makeStore from "./store";
+import App from "./App";
 import { webSocket } from "rxjs/webSocket";
 import { Subject } from 'rxjs';
 import { retryWhen, delay, tap } from 'rxjs/operators';
 import {QueueingSubject} from 'queueing-subject' 
 import Rsjx from "./Rsjx";
 
-ReactDOM.render(
-  <Rsjx />,
-  document.getElementById("root")
-);
-
 // ReactDOM.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>,
+//   <Rsjx />,
 //   document.getElementById("root")
 // );
+
+const store = makeStore();
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
