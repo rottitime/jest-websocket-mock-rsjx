8b7e4cd87c81a91dbce2f8629ad44902520030aa
Jaspaul Aggarwal <jaspaul.aggarwal@akinova.com>
2021-07-22 23:22:58 +0100

    passed test

--- examples/redux-saga/src/__tests__/Rsjx.test.js
+++ examples/redux-saga/src/__tests__/Rsjx.test.js
@@ -9,48 +9,30 @@ let server, client
 
 describe("The Rsjx component", () => {
 
-    beforeEach(() => {
-        server = new WS('ws://localhost:8080', {jsonProtocol: true});
-        global.Math.random = () => 123
+  beforeEach(async () => {
+      server = new WS('ws://localhost:8080', {jsonProtocol: true});
+      client = new WebSocket("ws://localhost:8080");
+      await server.connected;
+      global.Math.random = () => 123
+    });
+    afterEach(() => {
+      WS.clean();
+    });
 
-      });
-      afterEach(() => {
-        WS.clean();
-      });
-
-
-  it("rendered", async () => {
-    await render(<Rsjx />);
-    expect(screen.getByText("RSJX")).toBeInTheDocument();
-  })
 
     it("the server keeps track of received messages, and yields them as they come in", async () => {
-      let screen;
-        client = new WebSocket("ws://localhost:8080");
-        const messages = [];
-        client.onmessage = (e) => {
-          console.log('dedeeddedede', e.data)
-          messages.push(e.data);
-        };
-
-
         server.on('connection', socket => {
-          console.log('*OPENED')
           socket.on('message', data => {
-              console.log('*****************Server recieved', data)
+              // console.log('*****************Server recieved', data)
               act(() => {
                 /* fire events that update state */
                 socket.send(`{"server v2": ${data}}` );
-                // socket.send(`{"server v3": 1234}` );
-                // expect(screen).toMatchSnapshot();
               });
               
           });
         });
-
-        await server.connected;
    
-        screen = await render(<Rsjx />);
+        const screen = await render(<Rsjx />);
        
         await expect(server).toReceiveMessage('sender$ sent to server1: 123');
         expect(screen.getByText('recieved: {"server v2":"sender$ sent to server1: 123"}')).toBeInTheDocument();
@@ -58,22 +40,16 @@ describe("The Rsjx component", () => {
         expect(screen.getByText('recieved: {"server v2":"sender$ sent to server2: 123"}')).toBeInTheDocument();
         await expect(server).toReceiveMessage('sender$ sent to server3: 123');
         expect(screen.getByText('recieved: {"server v2":"sender$ sent to server3: 123"}')).toBeInTheDocument();
-
       });
 
 
+  it("rendered", async () => {
+    const screen = await render(<Rsjx />);
+    expect(screen.getByText("RSJX")).toBeInTheDocument();
+  })
 
-//   it("ws connected", async () => {
-//     await server.connected;    
-//     // await render(<Rsjx />);
-//     server.close();
 
-    
 
-//     expect(true).toBe(true)
-//     // ws.close();
-
-//   })
 
 //   it("sends the message when submitting the form", async () => {
 //     const { ws } = await render(<App />);
